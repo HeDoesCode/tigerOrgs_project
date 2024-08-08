7d0c9aeb72d644d618c0885c4dcdd4ff42ebba74
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-08 15:54:50 +0800

    Layout Update

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -4,7 +4,7 @@ export default function Welcome({ role }) {
 
     function IconBellFilled({ size }) {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-bell">
+            <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-bell">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
                 <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
@@ -24,11 +24,35 @@ export default function Welcome({ role }) {
 
     function IconMenu3({ size }) {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M4 6l16 0" />
                 <path d="M4 12l16 0" />
                 <path d="M4 18l16 0" />
+            </svg>
+        )
+    }
+
+    function IconList({ size }) {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-list-details">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M13 5h8" />
+                <path d="M13 9h5" />
+                <path d="M13 15h8" />
+                <path d="M13 19h5" />
+                <path d="M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+                <path d="M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+            </svg>
+        )
+    }
+
+    function IconHistory({ size }) {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-history">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 8l0 4l2 2" />
+                <path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
             </svg>
         )
     }
@@ -77,8 +101,8 @@ export default function Welcome({ role }) {
     );
 
     function Layout({ children, sidebar, footer, header }) {
-        const headerHeight_pt = 'pt-16';
-        const headerHeight_h = 'h-14';
+        const headerHeight_pt = 'pt-[4.5rem]';
+        const headerHeight_h = 'h-16';
         const sideBarWidth_pl = 'pl-20';
         const sideBarWidth_w = 'w-16';
         const footer_minHeight = 'min-h-32';
@@ -97,10 +121,16 @@ export default function Welcome({ role }) {
         function SideBar() {
             return (
                 <div className={`fixed flex flex-col left-0 ${sideBarWidth_w} h-full`}>
-                    <div className={`${headerHeight_h} flex items-center justify-center p-3`}>I.Menu</div>
-                    <nav className='flex flex-1 flex-col items-center p-3 border-gray-400 border-r-[1px]'>
-                        <Link>Orgs</Link>
-                        <Link>logs</Link>
+                    <div className={`${headerHeight_h} flex items-center justify-center`}>
+                        <IconMenu3 size={'27'} />
+                    </div>
+                    <nav className='flex flex-1 flex-col items-center py-5 border-gray-400 border-r-[1px] space-y-5'>
+                        <Link>
+                            <IconList />
+                        </Link>
+                        <Link>
+                            <IconHistory />
+                        </Link>
                     </nav>
                 </div>
             );
