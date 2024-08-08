32762ca832c84fe57f7eae7b4e285afe3503e426
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-08 15:23:50 +0800

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -2,6 +2,37 @@ import { Link, Head } from '@inertiajs/react';
 
 export default function Welcome({ role }) {
 
+    function IconBellFilled({ size }) {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-bell">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+                <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+            </svg>
+        )
+    }
+
+    function IconProfile({ size }) {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-circle">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+            </svg>
+        )
+    }
+
+    function IconMenu3({ size }) {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 6l16 0" />
+                <path d="M4 12l16 0" />
+                <path d="M4 18l16 0" />
+            </svg>
+        )
+    }
+
     return (
         <div className='w-full'>
             <Layout sidebar footer header> {/* pass array or object for sidebar links */}
@@ -37,12 +68,17 @@ export default function Welcome({ role }) {
     );
 
     function Layout({ children, sidebar, footer }) {
+        const headerHeight_pt = 'pt-14';
+        const headerHeight_h = 'h-14';
+        const sideBarWidth_pl = 'pl-20';
+        const sideBarWidth_w = 'w-16';
+        const footer_minHeight = 'min-h-32';
 
         return (
             <div>
                 <Header />
                 {sidebar && <SideBar />}
-                <main className={`w-full px-4 ${sidebar && `pl-16`} min-h-screen pt-14`}>
+                <main className={`w-full px-4 ${sidebar && sideBarWidth_pl} min-h-screen ${headerHeight_pt} b`}>
                     {children}
                 </main>
                 {footer && <Footer />}
@@ -51,8 +87,8 @@ export default function Welcome({ role }) {
 
         function SideBar() {
             return (
-                <div className={`fixed flex flex-col left-0 w-16 px-3`}>
-                    <div className={`h-14 flex items-center justify-center`}>I.Menu</div>
+                <div className={`fixed flex flex-col left-0 ${sideBarWidth_w} px-3`}>
+                    <div className={`${headerHeight_h} flex items-center justify-center`}>I.Menu</div>
                     <nav className='flex flex-col items-center'>
                         <Link>Orgs</Link>
                         <Link>logs</Link>
@@ -65,7 +101,7 @@ export default function Welcome({ role }) {
 
             return (
                 <div>
-                    <div className={`fixed left-0 right-0 top-0 flex flex-row items-center justify-between space-x-2 h-14 ${sidebar && (`pl-16`)} bg-[#EEEEEE] px-4`}>
+                    <div className={`fixed left-0 right-0 top-0 flex flex-row items-center justify-between space-x-2 ${headerHeight_h} ${sidebar && sideBarWidth_pl} bg-[#EEEEEE] px-4`}>
                         <div>TigerOrgs</div>
                         <nav className='flex-1'>
                             <ul className='flex justify-end space-x-4'>
@@ -76,26 +112,19 @@ export default function Welcome({ role }) {
                                     <div className='w-0 border-gray-400 border-r-[1px] h-5'></div>
                                 </li>
                                 <li><IconBellFilled /></li>
-                                <li>Profile</li>
+                                <li><IconProfile /></li>
                             </ul>
                         </nav>
                     </div>
                 </div>
             );
 
-            function IconBellFilled({ size }) {
-                return (
-                    <svg xmlns="http://www.w3.org/2000/svg" width={size || '24'} height={size || '24'} viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-bell">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14.235 19c.865 0 1.322 1.024 .745 1.668a3.992 3.992 0 0 1 -2.98 1.332a3.992 3.992 0 0 1 -2.98 -1.332c-.552 -.616 -.158 -1.579 .634 -1.661l.11 -.006h4.471z" />
-                        <path d="M12 2c1.358 0 2.506 .903 2.875 2.141l.046 .171l.008 .043a8.013 8.013 0 0 1 4.024 6.069l.028 .287l.019 .289v2.931l.021 .136a3 3 0 0 0 1.143 1.847l.167 .117l.162 .099c.86 .487 .56 1.766 -.377 1.864l-.116 .006h-16c-1.028 0 -1.387 -1.364 -.493 -1.87a3 3 0 0 0 1.472 -2.063l.021 -.143l.001 -2.97a8 8 0 0 1 3.821 -6.454l.248 -.146l.01 -.043a3.003 3.003 0 0 1 2.562 -2.29l.182 -.017l.176 -.004z" />
-                    </svg>
-                )
-            }
+
         }
 
         function Footer() {
             return (
-                <div className='b min-h-32 mx-4'>Footer</div>
+                <div className={`b ${footer_minHeight} mx-4`}>Footer</div>
             );
         }
     }
