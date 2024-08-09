2f2e9877fb331cfa8169b038f7b7cae7dfd88404
SeraphLaur <laurence.arcila@gmail.com>
2024-08-09 22:12:25 +0800

    Deleted Index

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,12 @@
-import { Head } from '@inertiajs/react';
-import UserLayout from '@/Layouts/UserLayout.jsx';
-import AdminLayout from '@/Layouts/AdminLayout';
+import { Head } from "@inertiajs/react";
+import UserLayout from "@/Layouts/UserLayout.jsx";
+import AdminLayout from "@/Layouts/AdminLayout";
 
 export default function Welcome() {
     return (
-        <div className='w-full'>
+        <div className="w-full">
             <Head title="Home" />
-            <UserLayout>
+            <AdminLayout>
                 content 1 <br />
                 content <br />
                 content <br />
@@ -43,7 +43,7 @@ export default function Welcome() {
                 content <br />
                 content <br />
                 content n <br />
-            </UserLayout>
+            </AdminLayout>
         </div>
-    )
+    );
 }
