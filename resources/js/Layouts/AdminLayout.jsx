2ab35f7773f6d4a9c94c48d97e1a27ac2e877fc6
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-09 16:56:42 +0800

    finished layout

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,10 +1,12 @@
 import IconExit from "@/Components/Icons/IconExit"
 import Layout from "./Layout"
 import { Link } from "@inertiajs/react"
+import IconUserSearch from '@/Components/Icons/IconUserSearch';
+import IconFolder from '@/Components/Icons/IconFolder';
 
 function AdminLayout({ children }) {
     return (
-        <Layout headerContent={<HeaderContent />} sidebarLinks={<SidebarLinks />} sidebar>
+        <Layout headerContent={<HeaderContent />} sidebar={<SideBarContent />}>
             {children}
         </Layout>
     )
@@ -17,8 +19,44 @@ function AdminLayout({ children }) {
         )
     }
 
-    function SidebarLinks() {
-        return (<></>)
+    function SideBarContent() {
+        return (
+            <div className={`border-gray-300 border-r-[1px] fixed left-0 top-0 bottom-0 min-w-16 w-16 max-w-52 flex flex-col justify-center bg-[#EEEEEE] transition-all ease-in-out duration-300 group hover:w-52 hover:bg-[#FEFEFE]`}>
+                <div className='flex'>
+                    <div className={`min-h-16 min-w-16 size-16 flex items-center justify-center p-2`}>
+                        {/* logo */}
+                        {/* <IconMenu3 size='27' /> */}
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPhcNkJ7-IxlXnLfMbPwT4l1LROZeDmxoO3A&s" alt="test" className='bg-cover rounded-full' />
+                    </div>
+                    <p className='text-center mr-3 font-bold text-xs leading-4 line-clamp-3 h-min my-auto w-full'>Placeholder Society of Information Technology Enthusiasts (SITE)</p>
+                </div>
+                <nav className='flex-1 flex flex-col space-y-3 m-2 transition-all group-hover:mr-0 ease-in-out duration-300'>
+                    <SideBarLink
+                        icon={<IconFolder size='100%' />}
+                        href='#managepage'
+                        desc='Manage'
+                        current />
+                    <SideBarLink
+                        icon={<IconUserSearch size='100%' />}
+                        href='#recruitmentpage'
+                        desc='Recruitment'
+                    />
+                </nav>
+            </div>
+        );
+
+        function SideBarLink({ icon, href, desc, current }) {
+            return (
+                <div className={`flex pl-[0.6rem] group-hover:transition ease-in-out duration-300 p-2 group-hover:pl-5 group-hover:rounded-r-none rounded-full ${current && 'bg-[#FFBC58]' || 'hover:bg-gray-800 hover:text-white'}`}>
+                    <Link className='contents' href={href}>
+                        <div className='min-h-7 min-w-7 size-7'>{icon}</div>
+                        <div className='text-left pl-3 text-lg overflow-hidden h-min my-auto w-full'>{desc}</div>
+                    </Link>
+                </div>
+            )
+        }
+
+
     }
 }
 
--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,12 +1,10 @@
 import { Link } from '@inertiajs/react';
-import IconMenu3 from '@/Components/Icons/IconMenu3';
-import IconList from '@/Components/Icons/IconList';
-import IconHistory from '@/Components/Icons/IconHistory';
-import IconTableOptions from '@/Components/Icons/IconTableOptions';
 import IconUserSearch from '@/Components/Icons/IconUserSearch';
+import IconFolder from '@/Components/Icons/IconFolder';
 
 function Layout({ children, sidebar, headerContent }) {
-    const headerHeight_pt = 'pt-[4.5rem]';
+    // const headerHeight_pt = 'pt-[4.5rem]';
+    const headerHeight_pt = 'pt-16';
     const headerHeight_h = 'h-16';
     const sideBarWidth_pl = 'pl-20';
     const sideBarWidth_w = 'w-16';
@@ -14,15 +12,14 @@ function Layout({ children, sidebar, headerContent }) {
     return (
         <div>
             <Header />
-            {sidebar && <SideBar />}
-            <main className={`w-full ${sidebar || 'p-4'} h-screen ${headerHeight_pt} overflow-y-auto`}>
+            {sidebar}
+            <main className={`w-full ${sidebar || 'p-4 overflow-y-auto'} h-screen ${headerHeight_pt}`}>
                 {/* {children} */}
                 {sidebar ?
                     <div className='flex h-full w-full'>
                         <div className={sideBarWidth_w}>
-                            {/* sidebar width */}
                         </div>
-                        <div className='flex-1 px-4'>{children}</div>
+                        <div className='flex-1 px-4 py-2 overflow-y-auto'>{children}</div>
                     </div>
                     :
                     <>{children}</>
@@ -31,48 +28,7 @@ function Layout({ children, sidebar, headerContent }) {
         </div>
     );
 
-    function SideBar() {
-        return (
-            // <div className={`fixed flex flex-col left-0 ${sideBarWidth_w} h-full`}>
-            //     {/* <div className='fixed inset-0 bg-gray-400 opacity-20'></div> */}
-            //     <div className={`${headerHeight_h} flex items-center justify-center w-full`}>
-            //         <IconMenu3 size={'27'} />
-            //     </div>
-            //     <div className='flex flex-1 w-full justify-center relative'>
-            //         <nav className='absolute h-min left-0 flex flex-col items-center p-5 border-gray-400 border-r-[1px] space-y-5 min-w-full bg-[#EEEEEE]'>
-            //             <SideBarLink desc='Organizations'>
-            //                 <IconList />
-            //             </SideBarLink>
-            //             <Link>
-            //                 <IconHistory />
-            //             </Link>
-            //             <Link>
-            //                 <IconTableOptions />
-            //             </Link>
-            //             <Link>
-            //                 <IconUserSearch size={27} />
-            //             </Link>
-            //         </nav>
-            //     </div>
-            // </div>
-            <div className={`fixed left-0 top-0 bottom-0 ${sideBarWidth_w} flex justify-center b`}>
-                <div className={`${headerHeight_h} ${sideBarWidth_w} flex items-center justify-center  b`}><IconMenu3 size='27' /></div>
-            </div>
-        );
 
-        // function SideBarLink({ children, desc }) {
-        //     return (<>
-        //         <Link>
-        //             <div className='flex space-x-3'>
-        //                 {children}
-        //                 <span className='w-0 hidden'>{desc}</span>
-        //             </div>
-        //         </Link>
-        //     </>)
-        // }
-
-
-    }
 
     function Header() {
         return (
