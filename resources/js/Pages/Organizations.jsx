c4e74a98127e402fbd0cf17d7f4ce54563afa6c7
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-11 23:39:07 +0800

    added orgs page

---
+++ resources/js/Pages/Organizations.jsx
@@ -0,0 +1,13 @@
+import UserLayout from "@/Layouts/UserLayout"
+
+function Organizations() {
+    return (
+        <div className="w-full">
+            <UserLayout>
+                <div className="w-full poppins text-xl font-bold">Browse <span className="text-[#ffb700]">Organizations</span></div>
+            </UserLayout>
+        </div>
+    )
+}
+
+export default Organizations
