f46663a22d7862869af843745d299cac3433dbf7
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-13 16:49:10 +0800

    Update Logo.jsx

--- resources/js/Components/Logo.jsx
+++ resources/js/Components/Logo.jsx
@@ -2,7 +2,7 @@ function Logo({ className, leftClass, rightClass }) {
     return (
         <div className={`poetsen-one text-2xl ${className} select-none`}>
             <span className={`text-[#E7A600] ${leftClass}`}>Tiger</span>
-            <span className={rightClass}>Orgss</span>
+            <span className={rightClass}>Orgs</span>
         </div>
     )
 }
