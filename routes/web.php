182fcb199262f3c7cf9f6f9436e8d3185c1af315
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-14 18:01:56 +0800

    Revert "Update web.php"
    This reverts commit 8dccb65844290a33dab18038b16b6a490411c5df.

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,25 @@ use phpseclib3\File\ASN1\Maps\OrganizationName;
 Route::get('/', function () {
     return Inertia::render('Home', [
         'bgImage' => asset('src/background/vecteezy_yellow-background-yellow-abstract-background-light-yellow_37153092.jpg'),
-        // 'canLogin' => Route::has('login'),
-        // 'canRegister' => Route::has('register'),
-        // 'laravelVersion' => Application::VERSION,
-        // 'phpVersion' => PHP_VERSION,
-        // 'isLoggedIn' => Auth::check(),
+        'tiger1' => asset('src/background/tiger1.png'),
+        'tiger2' => asset('src/background/tiger2.png'),
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+        'isLoggedIn' => Auth::check(),
     ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/organizations', function () {
+    return Inertia::render('Organizations');
+})->name('organizations');
 
 Route::get('/auth/google', [GoogleController::class, 'googlepage']);
 Route::get('/auth/google/callback', [GoogleController::class, 'googlecallback']);
 
+
 Route::middleware('auth')->group(function () {
-    Route::get('/organizations', function () {
-        return Inertia::render('Organizations');
-    })->name('organizations');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
