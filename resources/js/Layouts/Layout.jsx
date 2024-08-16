e74a27c4c1bc34ea7655e5746e27a5c60f68ceaf
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-16 13:52:51 +0800

    Update Layout.jsx
    added zindexes to fix user pages overlapping with layout

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -18,7 +18,9 @@ function Layout({ children, sidebar, headerContent, bgImage, footer }) {
     return (
         <div className={`${sidebar && "pl-0 sm:pl-16"} flex select-none`}>
             <Header />
-            {sidebar}
+            <div className="z-50">
+                {sidebar}
+            </div>
             <main
                 className={`w-full overflow-y-auto h-screen pt-[4.5rem] px-4 select-text flex flex-col`}
                 style={bgStyle}
@@ -33,9 +35,8 @@ function Layout({ children, sidebar, headerContent, bgImage, footer }) {
         return (
             <div className="relative">
                 <div
-                    className={`fixed left-0 right-0 top-0 flex flex-row items-center justify-between space-x-2 ${headerHeight_h} ${
-                        sidebar && sideBarWidth_pl
-                    } bg-[#EEEEEE] px-4 border-gray-400 border-b-[1px] z-50`}
+                    className={`fixed left-0 right-0 top-0 flex flex-row items-center justify-between space-x-2 ${headerHeight_h} ${sidebar && sideBarWidth_pl
+                        } bg-[#EEEEEE] px-4 border-gray-400 border-b-[1px] z-40`}
                 >
                     {/* <div className={`poetsen-one text-2xl ${sidebar && 'ml-2'}`}><span className='text-[#E7A600]'>Tiger</span>Orgs</div> */}
                     {/* <Link className='contents' href={route('/')}> */}
