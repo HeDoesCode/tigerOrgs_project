d3ef2ced7014f00e17668fd4fbc641d4b93e9efe
SeraphLaur <laurence.arcila@gmail.com>
2024-08-16 13:15:05 +0800

    Fixed

--- resources/js/Components/Organizations/OrganizationJoined.jsx
+++ resources/js/Components/Organizations/OrganizationJoined.jsx
@@ -1,16 +1,20 @@
-function OrganizationJoined({ icon, title, isAdmin, isSuperAdmin }) {
+import { Link } from "@inertiajs/react";
+function OrganizationJoined({ icon, title, isAdmin, isSuperAdmin, link }) {
     return (
-        <li className="flex items-center space-x-3 poppins">
-            <img
-                src={icon}
-                className="size-10 object-cover rounded-full"
-            />
-            <label className="flex-1 text-sm font-bold line-clamp-2 leading-4">{title}</label>
-            {(isAdmin || isSuperAdmin) &&
-                <label className="bg-red-600 text-white text-[0.6rem] px-[0.35rem] rounded-full">{(isAdmin && 'Admin') || (isSuperAdmin && 'S.Admin')}</label>
-            }
-        </li>
-    )
+        <Link href={link}>
+            <li className="flex items-center space-x-3 poppins">
+                <img src={icon} className="size-10 object-cover rounded-full" />
+                <label className="cursor-pointer flex-1 text-sm font-bold line-clamp-2 leading-4">
+                    {title}
+                </label>
+                {(isAdmin || isSuperAdmin) && (
+                    <label className="cursor-pointer bg-red-600 text-white text-[0.6rem] px-[0.35rem] rounded-full">
+                        {(isAdmin && "Admin") || (isSuperAdmin && "S.Admin")}
+                    </label>
+                )}
+            </li>
+        </Link>
+    );
 }
 
-export default OrganizationJoined
+export default OrganizationJoined;
