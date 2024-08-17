0444f2ab479b8d3c457f2b8eac2e72e0ee1ccaea
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-08-18 03:01:13 +0800

    Update Organizations.jsx
    added temp route

--- resources/js/Pages/Organizations/Organizations.jsx
+++ resources/js/Pages/Organizations/Organizations.jsx
@@ -103,7 +103,7 @@ function Organizations() {
                                 }
                                 title="Fotomasino 1"
                                 desc="The Thomasian Photographers Guild"
-                                href="#Fotomasino"
+                                href={route('organizations.home', ['test'])}
                             />
                             <OrganizationTile
                                 orgBg={
