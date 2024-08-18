78231742772796d22125c207054b623145787455
SeraphLaur <laurence.arcila@gmail.com>
2024-08-18 23:25:30 +0800

    Update SuperAdminInvite.jsx

--- resources/js/Pages/SuperAdmin/SuperAdminInvite.jsx
+++ resources/js/Pages/SuperAdmin/SuperAdminInvite.jsx
@@ -5,6 +5,8 @@ import IconInvite from "@/Components/Icons/IconInvite";
 import IconCheckBox from "@/Components/Icons/IconCheckBox";
 import IconDotsVertical from "@/Components/Icons/IconDotsVertical";
 import VerticalCard from "@/Components/VerticalCard";
+import AdminButton from "@/Components/Admin/AdminButton";
+import IconBellFilled from "@/Components/Icons/IconBellFilled";
 
 function SuperAdminInvite() {
     return (
@@ -26,35 +28,44 @@ function SuperAdminInvite() {
                     ]}
                     title="Role Invitation"
                 >
-                    <div className="grid grid-rows-1 p-5 gap-2">
-                        <VerticalCard gridcol="grid-cols-12">
-                            <div className=" col-span-3 content-center">
-                                <h1 className="ml-2 font-bold">
-                                    Joseph Paduga
-                                </h1>
-                            </div>
-                            <div className="col-span-3 content-center">
-                                <h1 className="text-center font-semibold text-gray-500">
-                                    laurencearvin.arcila.cics@ust.edu.ph
-                                </h1>
-                            </div>
-                            <div className="col-span-3 content-center">
-                                <h1 className=" text-center text-sm font-semibold text-gray-500">
-                                    College of Information and Computing
-                                    Sciences
-                                </h1>
-                            </div>
-                            <div className="col-span-2 px-4 text-sm content-center ">
-                                <h1 className=" text-center font-semibold rounded-xl poppins bg-green-50 border-2 border-green-600 text-green-800">
-                                    Assigned to 1 Org
-                                </h1>
-                            </div>
-                            <div className="col-span-1 flex justify-center">
-                                <div className="hover:bg-[#EEEEEE]  rounded-full">
-                                    <IconDotsVertical />
+                    <div>
+                        <div className="flex justify-end me-5 mt-5">
+                            <AdminButton
+                                className="mr-2 bg-gray-300 hover:bg-white"
+                                icon={<IconInvite />}
+                                name="Assign"
+                            />
+                        </div>
+                        <div className="grid grid-rows-1 p-5 gap-2">
+                            <VerticalCard gridcol="grid-cols-12">
+                                <div className=" col-span-3 content-center">
+                                    <h1 className="ml-2 font-bold">
+                                        Joseph Paduga
+                                    </h1>
                                 </div>
-                            </div>
-                        </VerticalCard>
+                                <div className="col-span-3 content-center">
+                                    <h1 className="text-center font-semibold text-gray-500">
+                                        laurencearvin.arcila.cics@ust.edu.ph
+                                    </h1>
+                                </div>
+                                <div className="col-span-3 content-center">
+                                    <h1 className=" text-center text-sm font-semibold text-gray-500">
+                                        College of Information and Computing
+                                        Sciences
+                                    </h1>
+                                </div>
+                                <div className="col-span-2 px-4 text-sm content-center ">
+                                    <h1 className=" text-center font-semibold rounded-xl poppins bg-green-50 border-2 border-green-600 text-green-800">
+                                        Assigned to 1 Org
+                                    </h1>
+                                </div>
+                                <div className="col-span-1 flex justify-center">
+                                    <div className="hover:bg-[#EEEEEE]  rounded-full">
+                                        <IconDotsVertical />
+                                    </div>
+                                </div>
+                            </VerticalCard>
+                        </div>
                     </div>
                 </MainAdminFrame>
             </SuperAdminLayout>
