05c842008eabc12133b12308ec3ffc0f05c6ac7e
Redvinz <arvinalkuino123@gmail.com>
2024-08-19 18:08:51 +0800

    Update TextInput.jsx

--- resources/js/Components/ui/TextInput.jsx
+++ resources/js/Components/ui/TextInput.jsx
@@ -3,13 +3,13 @@ function TextInput() {
     return (
         <div className="w-full p-4 m-1 bg-neutral-100 rounded-3xl">
             <span>
-                <div className="px-3 text-zinc-700 text-sm underline">
+                <div className="px-3 py-1 text-zinc-700 text-sm underline">
                     For short texts, use Text Field
                 </div>
             </span>
 
             <input
-                className="w-full bg-transparent rounded-2xl border-amber-300"
+                className="w-full bg-transparent rounded-2xl border-1 border-x-stone-600"
                 type="text"
                 placeholder="Question here..."
             />
