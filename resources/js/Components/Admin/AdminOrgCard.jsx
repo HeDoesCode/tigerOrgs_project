1663027473ad17ad07022426d0b7741388c4a9af
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-01 15:05:40 +0800

    Update AdminOrgCard.jsx

--- resources/js/Components/Admin/AdminOrgCard.jsx
+++ resources/js/Components/Admin/AdminOrgCard.jsx
@@ -10,7 +10,7 @@ function AdminOrgCard({ edit, visible, setVisible }) {
         : "bg-red-50  border-red-600 text-red-800";
 
     return (
-        <div className="hover:scale-[1.02] transition-all duration-300 ease-in-out px-4 rounded-xl divide-y divide-gray-300 shadow-lg bg-white hover:bg-gray-100">
+        <div className="hover:scale-[1.02] transition-all duration-300 ease-in-out px-4 rounded-xl divide-y divide-gray-300 shadow-lg bg-white hover:bg-gray-100 transOptimize">
             <div className="py-4 ">
                 <div className="grid grid-cols-3 divide-x divide-gray-300">
                     <div className="p-2 content-center">
