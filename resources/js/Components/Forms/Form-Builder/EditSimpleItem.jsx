19eea01bf93a5734ec19fe72858141af99013765
Redvinz <arvinalkuino123@gmail.com>
2024-09-09 20:46:38 +0800

    updates

--- resources/js/Components/Forms/Form-Builder/EditSimpleItem.jsx
+++ resources/js/Components/Forms/Form-Builder/EditSimpleItem.jsx
@@ -26,6 +26,21 @@ function EditSimpleItem({ id, type }) {
     return (
         <form onSubmit={handleSave}>
             <ul>
+                <li className="mb-2 rounded-2xl px-2">
+                    <input
+                        className="w-full bg-transparent rounded-xl border-[1.5px] border-x-stone-600"
+                        type="text"
+                        value={data.question}
+                        onChange={(e) => setData("question", e.target.value)}
+                        onBlur={(e) => {
+                            e.target.value === ""
+                                ? setData("question", defaultQuestion)
+                                : setData("question", e.target.value);
+                        }}
+                        placeholder="Type Question here..."
+                        required
+                    />
+                </li>
                 <div className="flex justify-end ">
                     <li className="flex items-center gap-2 m-2 px-2 rounded-2xl border-black size-fit  ">
                         <input
@@ -46,21 +61,6 @@ function EditSimpleItem({ id, type }) {
                     </li>
                 </div>
 
-                <li className="mb-2 rounded-2xl px-2">
-                    <input
-                        className="w-full bg-transparent rounded-xl border-[1.5px] border-x-stone-600"
-                        type="text"
-                        value={data.question}
-                        onChange={(e) => setData("question", e.target.value)}
-                        onBlur={(e) => {
-                            e.target.value === ""
-                                ? setData("question", defaultQuestion)
-                                : setData("question", e.target.value);
-                        }}
-                        placeholder="Type Question here..."
-                        required
-                    />
-                </li>
                 <li className="grid grid-cols-2">
                     {/* <button
                         className="bg-gray-200 px-4 py-2 border  hover:bg-gray-300 "
