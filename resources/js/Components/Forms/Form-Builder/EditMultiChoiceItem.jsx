1d9435cdab3cfd0c72a732858bb746e961b4ad12
SeraphLaur <laurence.arcila@gmail.com>
2024-09-09 22:47:59 +0800

    Revert "Merge branch 'Form_Builder_FrontEnd'"
    This reverts commit 7d78616f2c511a149cd8f82e291b784609b6d12e, reversing
    changes made to 0d03d25cb50e99919af2fa51d248921bb8f0afdb.

--- resources/js/Components/Forms/Form-Builder/EditMultiChoiceItem.jsx
+++ resources/js/Components/Forms/Form-Builder/EditMultiChoiceItem.jsx
@@ -50,6 +50,24 @@ function EditMultiChoiceItem({ id, type }) {
     return (
         <form onSubmit={handleSave}>
             <ul>
+                <div className="flex justify-end ">
+                    <li className="flex items-center gap-2 m-2 px-2 rounded-2xl border-black size-fit  ">
+                        <input
+                            className="rounded-2xl"
+                            type="checkbox"
+                            id={`required_${id}`}
+                            onChange={() => setData("required", !data.required)}
+                        />
+                        <label htmlFor={`required_${id}`}> Required</label>
+                        <label className="">|</label>
+                        <button
+                            className=" py-2 underline text-red-500 "
+                            onClick={() => handleDeleteItem(id)}
+                        >
+                            Delete Item
+                        </button>
+                    </li>
+                </div>
                 <li className="mb-2 rounded-2xl px-2">
                     <input
                         className="w-full bg-transparent rounded-xl border-1 border-x-stone-600"
@@ -110,24 +128,6 @@ function EditMultiChoiceItem({ id, type }) {
                         + Add Option
                     </button>
                 </li>
-                <div className="flex justify-end ">
-                    <li className="flex items-center gap-2 m-2 px-2 rounded-2xl border-black size-fit  ">
-                        <input
-                            className="rounded-2xl"
-                            type="checkbox"
-                            id={`required_${id}`}
-                            onChange={() => setData("required", !data.required)}
-                        />
-                        <label htmlFor={`required_${id}`}> Required</label>
-                        <label className="">|</label>
-                        <button
-                            className=" py-2 underline text-red-500 "
-                            onClick={() => handleDeleteItem(id)}
-                        >
-                            Delete Item
-                        </button>
-                    </li>
-                </div>
                 <li className="grid grid-cols-2">
                     {/* <button
                         className="bg-gray-200 px-4 py-2 border  hover:bg-gray-300 "
