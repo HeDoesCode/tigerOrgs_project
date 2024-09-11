1e9834d3f563c716259cc25c54a16294aee16d96
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-11 13:24:20 +0800

    Update OrganizationContainerRow.jsx
    added OrganizationContainerRow indicator at end of scroll

--- resources/js/Components/Organizations/OrganizationContainerRow.jsx
+++ resources/js/Components/Organizations/OrganizationContainerRow.jsx
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from "react"
 function OrganizationContainerRow({ children, title, className, index, collegeLength }) {
     const [expand, setExpand] = useState(false);
     const [hasHorizontalOverflow, setHasHorizontalOverflow] = useState(false);
+    const [isScrolledToEnd, setIsScrolledToEnd] = useState(false);
 
     const elementRef = useRef(null);
 
@@ -10,16 +11,26 @@ function OrganizationContainerRow({ children, title, className, index, collegeLe
         if (elementRef.current) {
             const isOverflowing = elementRef.current.scrollWidth > elementRef.current.clientWidth;
             setHasHorizontalOverflow(isOverflowing);
+
+            const scrollLeft = elementRef.current.scrollLeft;
+            const offsetWidth = elementRef.current.offsetWidth;
+            const scrollWidth = elementRef.current.scrollWidth;
+
+            const isAtEnd = Math.abs(scrollLeft + offsetWidth - scrollWidth) < 1;
+            setIsScrolledToEnd(isAtEnd);
         }
     };
 
     useEffect(() => {
+        const div = elementRef.current;
         checkOverflow();
 
         window.addEventListener('resize', checkOverflow);
+        div.addEventListener("scroll", checkOverflow);
 
         return () => {
             window.removeEventListener('resize', checkOverflow);
+            div.removeEventListener("scroll", checkOverflow);
         };
 
     }, []);
@@ -41,12 +52,12 @@ function OrganizationContainerRow({ children, title, className, index, collegeLe
 
 
     return (
-        <div className={`flex flex-col gap-y-3 w-full ${className} ${expand && expandSpacing(index)}`}>
+        <div className={`flex flex-col gap-y-3 w-full relative ${className} ${expand && expandSpacing(index)}`}>
             <div className="flex flex-row justify-between -mb-3">
                 <div className="questrial font-bold tracking-wider">{title}</div>
-                <div className="w-20 relative overflow-visible">
+                <div className="w-20 min-w-20 relative overflow-visible">
                     {(hasHorizontalOverflow || expand) && (
-                        <button onClick={expandRow} className={`absolute right-0 min-w-max underline text-sm py-1 px-2 hover:bg-gray-800 hover:text-white rounded-lg ${expand ? 'text-blue-500 font-bold' : 'text-gray-500'}`}>
+                        <button onClick={expandRow} className={`absolute right-0 min-w-max underline text-sm py-1 px-2 hover:bg-gray-800 hover:text-white rounded-lg ${expand ? 'text-blue-500 font-bold' : 'text-gray-500'} z-30`}>
                             {expand ? 'hide all' : 'show all'}
                         </button>
                     )}
@@ -57,11 +68,16 @@ function OrganizationContainerRow({ children, title, className, index, collegeLe
                     'w-full select-none max-h-full overflow-y-hidden overflow-x-hidden grid grid-cols-[repeat(auto-fill,_minmax(9rem,1fr))] md:grid-cols-[repeat(auto-fill,_minmax(12rem,1fr))] gap-4 justify-items-center rounded-lg border border-gray-300'
                 ) :
                 (
-                    'flex flex-row space-x-2 w-full overflow-x-auto overflow-y-hidden'
+                    `flex flex-row space-x-2 w-full overflow-x-auto overflow-y-hidden`
                 )
             }>
                 {children}
+
             </div>
+            {!expand && hasHorizontalOverflow && (
+                <div className={`absolute right-0 top-6 bottom-0 w-20 bg-gradient-to-r from-transparent to-[#EEEEEE]/70 pointer-events-none transition-all ease-in ${isScrolledToEnd && '!w-0'}`}>
+                </div>
+            )}
         </div >
     )
 }
