950bb03917f02e05e2c7124b67b5070e181227ff
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-14 02:42:02 +0800

    custom error UI

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,7 +2,4 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
-
-
-
+@section('message', __($exception->getMessage() ?: 'Sorry, you are not allowed to access this page'))
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', __('Server Error'))
+@section('message', __('An unexpected error occurred on our end. Please try again later'))
