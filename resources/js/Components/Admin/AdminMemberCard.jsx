f00c925dc1e0761ce2a3e65422fb67df960d17f8
Redvinz <arvinalkuino123@gmail.com>
2024-09-13 13:41:03 +0800

    admin_controller

--- resources/js/Components/Admin/AdminMemberCard.jsx
+++ resources/js/Components/Admin/AdminMemberCard.jsx
@@ -10,14 +10,22 @@ import VerticalCard from "@/Components/VerticalCard";
 import IconSave from "@/Components/Icons/IconSave";
 import IconCancel from "@/Components/Icons/IconCancel";
 
-function AdminMemberCard({ isAdmin }) {
+function AdminMemberCard({
+    isAdmin,
+    name,
+    position,
+    college,
+    email,
+    showActions = true,
+}) {
     return (
         <div className="hover:scale-[1.02] transition-all duration-300 ease-in-out shadow-lg hover:bg-gray-100 p-3 w-full bg-white rounded-xl">
-            <div className="flex justify justify-between mb-4">
+            {/* Admin or Member Badge */}
+            <div className="flex justify-between mb-4">
                 <div
                     className={`${
                         isAdmin ? "bg-[#FF9900]" : "bg-[#FFBC58]"
-                    } p-1 px-4 rounded-2xl flex justify-between`}
+                    } p-1 px-4 rounded-2xl flex justify-between items-center`}
                 >
                     <div className="mx-1 text-gray-800">
                         <IconAdmin />
@@ -26,28 +34,36 @@ function AdminMemberCard({ isAdmin }) {
                         {isAdmin ? "Admin" : "Member"}
                     </div>
                 </div>
-                <div className="flex">
-                    <div className="mx-1 text-gray-500">
-                        <IconEdit />
+
+                {/* Conditionally show Edit/Delete buttons */}
+                {showActions && (
+                    <div className="flex">
+                        <div className="mx-1 text-gray-500 cursor-pointer">
+                            <IconEdit />
+                        </div>
+                        <div className="mx-1 text-gray-500 cursor-pointer">
+                            <IconDelete />
+                        </div>
                     </div>
-                    <div className="mx-1 text-gray-500">
-                        <IconDelete />
-                    </div>
-                </div>
+                )}
             </div>
+
+            {/* Member Details */}
             <div className="grid grid-cols-5 mb-1">
                 <div className="col-span-1 grid content-center justify-self-center text-gray-500">
                     <IconProfile />
                 </div>
-                <div className="col-span-4 text-lg font-bold">Roshe Sapin</div>
+                <div className="col-span-4 text-lg font-bold truncate">
+                    {name}
+                </div>
             </div>
 
             <div className="grid grid-cols-5 mb-1">
                 <div className="col-span-1 grid content-center justify-self-center text-gray-500">
                     <IconPosition />
                 </div>
-                <div className="col-span-4 text-lg font-semibold">
-                    President
+                <div className="col-span-4 text-lg font-semibold truncate">
+                    {position}
                 </div>
             </div>
 
@@ -55,18 +71,14 @@ function AdminMemberCard({ isAdmin }) {
                 <div className="col-span-1 grid content-center justify-self-center text-gray-500">
                     <IconDepartment />
                 </div>
-                <div className="col-span-4 text-md ">
-                    College of Information and Computing Sciences (CICS)
-                </div>
+                <div className="col-span-4 text-md truncate">{college}</div>
             </div>
 
             <div className="grid grid-cols-5 mb-1">
                 <div className="col-span-1 grid content-center justify-self-center text-gray-500">
                     <IconEmail />
                 </div>
-                <div className="col-span-4 text-md truncate">
-                    roshe.sapin.cics@ust.edu.ph
-                </div>
+                <div className="col-span-4 text-md truncate">{email}</div>
             </div>
         </div>
     );
