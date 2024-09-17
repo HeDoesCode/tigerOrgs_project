0a8153ceda13d3792081ba91862f2c2dafe5aa07
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-17 23:23:09 +0800

    Create SuperAdminLoginHistorySeeder.php

---
+++ database/seeders/SuperAdminLoginHistorySeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use DateTime;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class SuperAdminLoginHistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // [
+        //     'userID' => '2024000004',
+        //     'email' => 'josephvictor.paduga.cics@ust.edu.ph',
+        //     'firstname' => 'joseph victor',
+        //     'lastname' => 'paduga',
+        //     'middlename' => 'CICS',
+        //     'college' => 'College of Information and Computing Sciences',
+        //     'status' => 'student',
+        // ]
+
+        $records = [];
+        $startDate = new DateTime('2024-01-01');
+        for ($i = 1; $i < 501; $i++) {
+            $records[] = [
+                'loginID' => $i,
+                'userID' => '2024000004',
+                'login_time' => $startDate->format('Y-m-d H:i:s'),
+            ];
+
+            $startDate->modify('+1 day');
+        }
+
+        DB::table('superadmin_login_history')->insert($records);
+    }
+}
