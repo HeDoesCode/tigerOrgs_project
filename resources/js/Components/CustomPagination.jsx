31f996b0cab98bae373d5112b0aca39538b8c9b9
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-19 23:33:01 +0800

    ui update

--- resources/js/Components/CustomPagination.jsx
+++ resources/js/Components/CustomPagination.jsx
@@ -34,13 +34,13 @@ function CustomPagination({ page }) {
         <div className="max-w-full w-fit flex justify-between shadow-md shadow-black/40 py-1 bg-[#EEEEEE] rounded-full overflow-clip px-1">
             <Pagination className='contents'>
                 <PaginationContent className='contents'>
-                    <div className="w-fit my-auto">
-                        <PaginationItem>
-                            <PaginationPrevious
-                                onClick={() => handlePaging(page.links[0].url)}
-                                className={`rounded-l-full ${page.current_page === 1 && 'cursor-not-allowed hover:bg-transparent'}`} />
-                        </PaginationItem>
-                    </div>
+                    {/* <div className="w-fit my-auto"> */}
+                    <PaginationItem>
+                        <PaginationPrevious
+                            onClick={() => handlePaging(page.links[0].url)}
+                            className={`rounded-l-full h-full ${page.current_page === 1 && 'cursor-not-allowed hover:bg-transparent'}`} />
+                    </PaginationItem>
+                    {/* </div> */}
 
                     <div className="flex flex-row flex-1 overflow-x-auto">
                         {page.links.map((link, index) => {
@@ -67,14 +67,14 @@ function CustomPagination({ page }) {
                         })}
                     </div>
 
-                    <div className="w-fit my-auto">
-                        <PaginationItem>
-                            <PaginationNext
-                                onClick={() => handlePaging(page.links[page.links.length - 1].url)}
-                                className={`rounded-r-full ${page.current_page === page.last_page && 'cursor-not-allowed hover:bg-transparent'}`}
-                            />
-                        </PaginationItem>
-                    </div>
+                    {/* <div className="w-fit my-auto"> */}
+                    <PaginationItem>
+                        <PaginationNext
+                            onClick={() => handlePaging(page.links[page.links.length - 1].url)}
+                            className={`rounded-r-full h-full ${page.current_page === page.last_page && 'cursor-not-allowed hover:bg-transparent'}`}
+                        />
+                    </PaginationItem>
+                    {/* </div> */}
                 </PaginationContent>
             </Pagination>
         </div>
