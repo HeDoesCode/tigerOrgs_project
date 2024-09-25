87bb6c525b8bc12edb632f6a67986399c983a710
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-09-25 11:48:31 +0800

    update svgs to react format

--- resources/js/Components/Icons/number123.jsx
+++ resources/js/Components/Icons/number123.jsx
@@ -9,9 +9,9 @@ function number123() {
          viewBox="0 0 24 24"
          fill="none"
          stroke="currentColor"
-         stroke-width="2"
-         stroke-linecap="round"
-         stroke-linejoin="round"
+         strokeWidth="2"
+         strokeLinecap="round"
+         strokeLinejoin="round"
          className="icon icon-tabler icons-tabler-outline icon-tabler-number-123"
       >
          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
