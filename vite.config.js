fb3e95372a9d4642fa088a374529981fb01c917e
SeraphLaur <laurence.arcila@gmail.com>
2024-10-19 12:42:44 +0800

    Push to main

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             // input: 'resources/js/app.jsx',
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.jsx",
-                "resources/js/echo.js",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
