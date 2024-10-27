f60a21b3e3fb634f2eb64118a9eda70122d2111d
SeraphLaur <laurence.arcila@gmail.com>
2024-10-27 15:32:05 +0800

    Draft layout

--- resources/js/Pages/Admin/AdminManageApplication.jsx
+++ resources/js/Pages/Admin/AdminManageApplication.jsx
@@ -41,35 +41,40 @@ function AdminManageApplication({ orgID }) {
                     ]}
                     title="Manage Student Applications"
                 >
-                    <div className="p-5">
-                        <div className="poppins">Positions Available:</div>
-                        <div className="bg-[#EEEEEE] mt-2 border min-h-24 p-4 border-gray-400 rounded-xl flex flex-wrap ">
-                            <div className="mb-5 flex items-center">
-                                <input
-                                    type="radio"
-                                    name="position"
-                                    value="President"
-                                    defaultChecked
-                                    className="ml-5 mr-2 "
-                                    style={{}}
-                                />
-                                <h1>President</h1>
-                                <div className="ml-2 mr-2 rounded-full p-1 text-sm text-center  font-bold bg-gray-300">
+
+                    <div className=" grid grid-cols-12 divide-x min-h-full divide-gray-200">
+                        <div className="col-span-2 "> 
+                            <div className="poppins p-5">Forms Available:</div>
+                            
+                        
+                        <div className=" p-4 rounded-l-xl  ml-3 bg-white flex"><h1 className="font-extrabold">Staff for Back-End</h1> <div className="ml-2 mr-2 rounded-full p-1 text-sm text-center  font-bold bg-gray-300">
                                     21
                                 </div>
-                            </div>
+                                </div>
+                                <div className=" p-4 rounded-l-xl  ml-3 hover:bg-white flex"><h1 className="font-extrabold">Staff for Front-End</h1> <div className="ml-2 mr-2 rounded-full p-1 text-sm text-center  font-bold bg-gray-300">
+                                    21
+                                </div></div>
+                                <div className=" p-4 rounded-l-xl  ml-3 hover:bg-white flex"><h1 className="font-extrabold">Staff for Front-End</h1> <div className="ml-2 mr-2 rounded-full p-1 text-sm text-center  font-bold bg-gray-300">
+                                    21
+                                </div></div>
+                                <div className=" p-4 rounded-l-xl  ml-3 hover:bg-white flex"><h1 className="font-extrabold">Staff for Front-End</h1> <div className="ml-2 mr-2 rounded-full p-1 text-sm text-center  font-bold bg-gray-300">
+                                    21
+                                </div></div>
                         </div>
+                        
 
-                        <div className="poppins mt-5">
-                            Application Responses for position{" "}
+                        <div className="col-span-10 ">
+                            <div className="poppins">
+                            <h1 className="p-5">Application Responses for position{" "}
                             <span className="underline underline-offset-2">
                                 President
                             </span>
-                            :
+                            :</h1>
+                            
                         </div>
 
-                        <div className="bg-[#EEEEEE] mt-2 border divide-y  divide-gray-400  border-gray-400 rounded-xl   ">
-                            <div className="lg:grid hidden  grid-cols-9 py-2 text-center font-medium">
+                        <div className="mr-3  mb-3 bg-white divide-y min-h-96    rounded-r-xl divide-gray-200    ">
+                            <div className="lg:grid hidden font-bold grid-cols-9 p-4 text-center  ">
                                 <div className="col-span-2 ">Full Name</div>
                                 <div className="col-span-2">College</div>
                                 <div className="col-span-2">Email</div>
@@ -79,7 +84,7 @@ function AdminManageApplication({ orgID }) {
                                 </div>
                                 <div className="col-span-1">Actions</div>
                             </div>
-                            <div className="grid grid-cols-1 lg:grid-cols-9 py-2 text-center min-h-16">
+                            <div><div className="grid grid-cols-1 lg:grid-cols-9 py-2 text-center min-h-16">
                                 <div className="lg:col-span-2 content-center">
                                     Laurence Arvin Arcilla
                                 </div>
@@ -133,7 +138,185 @@ function AdminManageApplication({ orgID }) {
                                     />
                                 </div>
                             </div>
+                            </div>
+
+                            <div><div className="grid grid-cols-1 lg:grid-cols-9 py-2 text-center min-h-16">
+                                <div className="lg:col-span-2 content-center">
+                                    Laurence Arvin Arcilla
+                                </div>
+                                <div className="lg:col-span-2 content-center ">
+                                    College of Information and Computing
+                                    Sciences
+                                </div>
+                                <div className="lg:col-span-2 content-center truncate">
+                                    laurencearvin.arcilla.cics@ust.edu.ph
+                                </div>
+                                <div className="col-span-1 content-center">
+                                    Aug-13-2024
+                                </div>
+                                <div className="lg:col-span-1 px-4 content-center">
+                                    <div
+                                        className={`bg-[#609B00] rounded-xl text-white`}
+                                    >
+                                        50% Match
+                                    </div>
+                                </div>
+                                <div className="col-span-1 grid grid-cols-2">
+                                    <AdminDialog
+                                        title="Answer of the Applicant"
+                                        trigger={
+                                            <div className="underline content-center underline-offset-2">
+                                                View{" "}
+                                                <span className="lg:hidden ">
+                                                    Response{" "}
+                                                </span>
+                                            </div>
+                                        }
+                                    ></AdminDialog>
+
+                                    <AdminDropdownMenu
+                                        triggerContent={<DotsVertical />}
+                                        title="Select Action"
+                                        dropdownItems={[
+                                            {
+                                                name: "Assign as President (First Choice)",
+                                                value: "assign",
+                                            },
+                                            {
+                                                name: "Send Notification",
+                                                value: "notif",
+                                            },
+                                            {
+                                                name: "Reject Application",
+                                                value: "reject",
+                                            },
+                                        ]}
+                                    />
+                                </div>
+                            </div>
+                            </div>
+
+
+
+                            <div><div className="grid grid-cols-1 lg:grid-cols-9 py-2 text-center min-h-16">
+                                <div className="lg:col-span-2 content-center">
+                                    Laurence Arvin Arcilla
+                                </div>
+                                <div className="lg:col-span-2 content-center ">
+                                    College of Information and Computing
+                                    Sciences
+                                </div>
+                                <div className="lg:col-span-2 content-center truncate">
+                                    laurencearvin.arcilla.cics@ust.edu.ph
+                                </div>
+                                <div className="col-span-1 content-center">
+                                    Aug-13-2024
+                                </div>
+                                <div className="lg:col-span-1 px-4 content-center">
+                                    <div
+                                        className={`bg-[#609B00] rounded-xl text-white`}
+                                    >
+                                        50% Match
+                                    </div>
+                                </div>
+                                <div className="col-span-1 grid grid-cols-2">
+                                    <AdminDialog
+                                        title="Answer of the Applicant"
+                                        trigger={
+                                            <div className="underline content-center underline-offset-2">
+                                                View{" "}
+                                                <span className="lg:hidden ">
+                                                    Response{" "}
+                                                </span>
+                                            </div>
+                                        }
+                                    ></AdminDialog>
+
+                                    <AdminDropdownMenu
+                                        triggerContent={<DotsVertical />}
+                                        title="Select Action"
+                                        dropdownItems={[
+                                            {
+                                                name: "Assign as President (First Choice)",
+                                                value: "assign",
+                                            },
+                                            {
+                                                name: "Send Notification",
+                                                value: "notif",
+                                            },
+                                            {
+                                                name: "Reject Application",
+                                                value: "reject",
+                                            },
+                                        ]}
+                                    />
+                                </div>
+                            </div>
+                            </div>
+
+                            <div><div className="grid grid-cols-1 lg:grid-cols-9 py-2 text-center min-h-16">
+                                <div className="lg:col-span-2 content-center">
+                                    Laurence Arvin Arcilla
+                                </div>
+                                <div className="lg:col-span-2 content-center ">
+                                    College of Information and Computing
+                                    Sciences
+                                </div>
+                                <div className="lg:col-span-2 content-center truncate">
+                                    laurencearvin.arcilla.cics@ust.edu.ph
+                                </div>
+                                <div className="col-span-1 content-center">
+                                    Aug-13-2024
+                                </div>
+                                <div className="lg:col-span-1 px-4 content-center">
+                                    <div
+                                        className={`bg-[#609B00] rounded-xl text-white`}
+                                    >
+                                        50% Match
+                                    </div>
+                                </div>
+                                <div className="col-span-1 grid grid-cols-2">
+                                    <AdminDialog
+                                        title="Answer of the Applicant"
+                                        trigger={
+                                            <div className="underline content-center underline-offset-2">
+                                                View{" "}
+                                                <span className="lg:hidden ">
+                                                    Response{" "}
+                                                </span>
+                                            </div>
+                                        }
+                                    ></AdminDialog>
+
+                                    <AdminDropdownMenu
+                                        triggerContent={<DotsVertical />}
+                                        title="Select Action"
+                                        dropdownItems={[
+                                            {
+                                                name: "Assign as President (First Choice)",
+                                                value: "assign",
+                                            },
+                                            {
+                                                name: "Send Notification",
+                                                value: "notif",
+                                            },
+                                            {
+                                                name: "Reject Application",
+                                                value: "reject",
+                                            },
+                                        ]}
+                                    />
+                                </div>
+                            </div>
+                            </div>
+                            
                         </div>
+                        
+                        
+                        </div>
+                        
+
+                        
                     </div>
                 </MainAdminFrame>
             </AdminLayout>
