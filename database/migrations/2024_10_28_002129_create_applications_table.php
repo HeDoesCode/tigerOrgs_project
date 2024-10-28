9af6a879fdfe695afa748324e554949567b74b15
SeraphLaur <laurence.arcila@gmail.com>
2024-10-28 11:09:12 +0800

    Submit applications and store to database

---
+++ database/migrations/2024_10_28_002129_create_applications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id('applicationID');
+            $table->string('userID');
+            $table->foreign('userID')->references('userID')->on('users')->onDelete('cascade');
+            $table->foreignId('orgID')->constrained('organizations', 'orgID')->onDelete('cascade');
+            $table->foreignId('formID')->constrained('forms', 'formID')->onDelete('cascade');
+            //add criteria column na nullable?
+            $table->json('userData')->nullable();//nullable muna for now
+            $table->float('similarityScore')->nullable();//nullable muna for now
+            $table->string('additionalInstructions')->nullable();
+            $table->enum('status', ['submitted', 'accepted', 'pending', 'rejected'])->default('submitted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
--- resources/js/Pages/Organizations/Apply.jsx
+++ resources/js/Pages/Organizations/Apply.jsx
@@ -1,15 +1,21 @@
-import FormRenderer from "@/Components/Forms/Form-Renderer/FormRenderer"
-import OrganizationLayout from "@/Components/Organizations/OrganizationLayout"
-import { Head } from "@inertiajs/react"
+import FormRenderer from "@/Components/Forms/Form-Renderer/FormRenderer";
+import OrganizationLayout from "@/Components/Organizations/OrganizationLayout";
+import { Head } from "@inertiajs/react";
 
-function Apply({ pageLayoutData, formLayout }) {
-    console.log(formLayout)
+function Apply({ pageLayoutData, formLayout, orgID, formID }) {
+    console.log(formLayout);
     return (
         <OrganizationLayout pageLayoutData={pageLayoutData}>
-            <Head title={`Applying for ${pageLayoutData.metadata.organizationName}`} />
-            <FormRenderer formLayout={formLayout} />
+            <Head
+                title={`Applying for ${pageLayoutData.metadata.organizationName}`}
+            />
+            <FormRenderer
+                orgID={orgID}
+                formID={formID}
+                formLayout={formLayout}
+            />
         </OrganizationLayout>
-    )
+    );
 }
 
-export default Apply
+export default Apply;
