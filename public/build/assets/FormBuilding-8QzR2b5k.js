ba90b51f385b35880b1d17185d8c1b32aad789db
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-11-08 00:28:25 +0800

    build

--- public/build/assets/FormBuilding-8QzR2b5k.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-H8joi_T6.js";import{F as i}from"./FormBuilder-C05YKEJp.js";function m(){return r.jsx("div",{children:r.jsx(i,{})})}export{m as default};
--- public/build/assets/Forms-DaWDAgUP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-H8joi_T6.js";import{F as t}from"./FormRenderer-DH4DKuCE.js";function s({formLayout:r}){return o.jsx(t,{formLayout:r})}export{s as default};
