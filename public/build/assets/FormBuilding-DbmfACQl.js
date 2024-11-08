307b05281dea1408345fb5b6ad5cf832191f5b04
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-11-08 15:21:26 +0800

    build

--- public/build/assets/FormBuilding-DbmfACQl.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DNqcEfqS.js";import{F as i}from"./FormBuilder-Bp8vxYq-.js";function m(){return r.jsx("div",{children:r.jsx(i,{})})}export{m as default};
--- public/build/assets/Forms-B-cgHIGM.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-DNqcEfqS.js";import{F as t}from"./FormRenderer-gDJpd0gY.js";function s({formLayout:r}){return o.jsx(t,{formLayout:r})}export{s as default};
