61a35a77c30fa39f73dbb82d94912a424ac5aa26
SeraphLaur <laurence.arcila@gmail.com>
2024-11-10 21:40:03 +0800

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -22,6 +22,10 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): Response
     {
+        if (Auth::check()) {
+            return redirect()->route('/');
+        }
+        
         return Inertia::render('Home', [
             'bgImage' => asset('src/background/vecteezy_yellow-background-yellow-abstract-background-light-yellow_37153092.jpg'),
             'tiger1' => asset('src/background/tiger1.png'),
