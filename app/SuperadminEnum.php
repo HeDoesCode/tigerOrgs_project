3b4989103fcf93de1774e47ce36555cbceb2e31f
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-11-10 22:18:21 +0800

    Update SuperadminEnum.php

--- app/SuperadminEnum.php
+++ app/SuperadminEnum.php
@@ -7,7 +7,7 @@ enum SuperadminEnum: string
     public static function emails(): array
     {
         return [ // replace all with single email (OSA) on deployment
-            'josephvictor.paduga.cics@ust.edu.ph',
+            // 'josephvictor.paduga.cics@ust.edu.ph',
             'test.email.cics@ust.edu.ph',
             // 'arvin.alkuino.cics@ust.edu.ph',
             // 'laurencearvin.arcilla.cics@ust.edu.ph',
