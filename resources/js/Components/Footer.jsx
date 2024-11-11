1af7b76063467198274051d7f7b352aff0764295
Redvinz <arvinalkuino123@gmail.com>
2024-11-11 23:38:13 +0800

    Update Footer.jsx

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            Office for Student Affairs.
+                            UST Office for Student Affairs.
                         </a>
                         <br />
                         <br />
