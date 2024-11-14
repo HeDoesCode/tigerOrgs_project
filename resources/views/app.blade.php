664ec3a7f3503a34363a37bb92e66d61c1153f2f
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-11-14 13:12:42 +0800

    Reapply "remove feature"
    This reverts commit f04f37c1f1c5ecdef12903185c39d2d5d49e9af3.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {!! \App\Meta::render() !!}
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
