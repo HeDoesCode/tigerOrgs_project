6b3d16899ad9f71ae9d0159b082faa49705e2238
Redvinz <arvinalkuino123@gmail.com>
2024-11-26 22:10:10 +0800

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -17,83 +17,75 @@ use Laravel\Socialite\Two\InvalidStateException;
 class GoogleController extends Controller
 {
     public function googlepage(Request $request)
-    {
-        session()->put('remember_me', request('remember_me'));
-        return Socialite::driver('google')->with([
-            'prompt' => 'select_account',
-            'hd' => 'ust.edu.ph',
-            'hl' => 'en'
-        ])->redirect();
-    }
-    
+{
+session()->put('remember_me', request('remember_me'));
 
-    public function googlecallback(Request $request)
-    {
-        try {
-            $socialiteUser = Socialite::driver('google')->user();
-
-            // $controller = new AuthenticatedSessionController;
-            // return $controller->store($socialiteUser);
-        } catch (InvalidStateException $e) {
-
-            session()->flash('toast', [
-                'title' => 'Authentication session expired.',
-                'description' => 'Please try again.',
-                'variant' => 'destructive'
-            ]);
-
-            // wag destroy session mawawala yung toast message
-
-            return redirect()->route('login');
-        }
-
-        if (SuperadminEnum::check($socialiteUser->email)) {
-            if (!User::where('email', $socialiteUser->email)->first()) {
-                User::firstOrCreate(
-                    ['email' => $socialiteUser->email],
-                    [
-                        'userID' => '0000000001',
-                        'firstname' => $socialiteUser->user['given_name'],
-                        'lastname' => $socialiteUser->user['family_name'],
-                        'status' => 'osa',
-                        'college' => 'osa'
-                    ]
-                );
-            }
-
-            Auth::login(
-                User::where('email', $socialiteUser->email)->first(),
-                session()->pull('remember_me', 'false')
-            );
-
-            request()->session()->regenerate();
-            return redirect()->route('superadmin.index');
-        }
+if (env('APP_ENV') === "local") {
+return $this->mockGooglecallback();
+}
+return Socialite::driver('google')->with([
+'prompt' => 'select_account',
+'hd' => 'ust.edu.ph',
+'hl' => 'en'
+])->redirect();
+}
 
 
-        if (AppSettings::isManualRegistration()) {
-            $registeredUserExists = User::where('email', $socialiteUser->email)->exists();
+public function mockGooglecallback() {
 
-            if ($registeredUserExists) { // if student exists in DB, redirect to index
-                $registeredUser = User::where('email', $socialiteUser->email)->firstOrFail();
+$socialiteUser = new \stdClass();
+$socialiteUser->email = env('APP_TEST_EMAIL');
+$socialiteUser->given_name = env('APP_TEST_GIVEN_NAME');
+$socialiteUser->family_name = env('APP_TEST_FAMILY_NAME');
 
-                Auth::login($registeredUser, session()->pull('remember_me', 'false'));
-                request()->session()->regenerate();
-                return redirect()->intended('/');
-            }
+if (SuperadminEnum::check($socialiteUser->email)) {
+if (!User::where('email', $socialiteUser->email)->first()) {
+User::firstOrCreate(
+['email' => $socialiteUser->email],
+[
+'userID' => '0000000001',
+'firstname' => $socialiteUser->user['given_name'],
+'lastname' => $socialiteUser->user['family_name'],
+'status' => 'osa',
+'college' => 'osa'
+]
+);
+}
 
-            session()->put('socialiteUser', $socialiteUser);
-            return redirect()->route('register');
-        } else {
-            $registeredUser = User::where('email', $socialiteUser->email)->first();
+Auth::login(
+User::where('email', $socialiteUser->email)->first(),
+session()->pull('remember_me', 'false')
+);
 
-            if (!$registeredUser) {
-                abort(401, 'You are not permitted to access this application.');
-            }
+request()->session()->regenerate();
+return redirect()->route('superadmin.index');
+}
 
-            Auth::login($registeredUser, session()->pull('remember_me', false));
-            request()->session()->regenerate();
-            return redirect()->intended('/');
-        }
-    }
+
+if (AppSettings::isManualRegistration()) {
+$registeredUserExists = User::where('email', $socialiteUser->email)->exists();
+
+if ($registeredUserExists) { // if student exists in DB, redirect to index
+$registeredUser = User::where('email', $socialiteUser->email)->firstOrFail();
+
+Auth::login($registeredUser, session()->pull('remember_me', 'false'));
+request()->session()->regenerate();
+return redirect()->intended('/');
+}
+
+session()->put('socialiteUser', $socialiteUser);
+return redirect()->route('register');
+} else {
+$registeredUser = User::where('email', $socialiteUser->email)->first();
+
+if (!$registeredUser) {
+abort(401, 'You are not permitted to access this application.');
+}
+
+Auth::login($registeredUser, session()->pull('remember_me', false));
+request()->session()->regenerate();
+return redirect()->intended('/');
+}
+
+}
 }
